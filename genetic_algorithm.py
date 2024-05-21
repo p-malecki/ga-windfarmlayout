71f029f4d37f745b339d5d75f9f26b5d53477fbf
Paweł Małecki <p.malecki2002@gmail.com>
2024-05-22 00:18:48 +0200

    Refactor main GA

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,12 +1,10 @@
-import random
-import math
-import numpy as np
-from fitness import evaluate_fitness
+import matplotlib.pyplot as plt
+
+from fitness import evaluate_fitness, calculate_layout_energy_production
 from selection import selection
 from crossover import crossover
 from mutation import mutation
 from replacement import replacement
-from constraints import *
 from utils import *
 from plotting import *
 
@@ -15,32 +13,10 @@ def initialize_population(population_size, n_turbines, area_size, min_spacing, m
     """Generate random turbine layouts with each turbine within the land area and satisfying spacing constraints."""
 
     initial_population = []
-    max_coordinate = area_size  # - min_spacing
     num_bits = determine_num_bits(area_size)
-
     for _ in range(population_size):
-        layout = []
-        for _ in range(n_turbines):
-            x = random.randint(0, max_coordinate)
-            y = random.randint(0, max_coordinate)
-
-            attempts = 0
-            while attempts < max_attempts:
-                x = random.randint(0, max_coordinate)
-                y = random.randint(0, max_coordinate)
-
-                is_new_position_valid = True
-                for pos in layout:
-                    if not is_valid_spacing(pos, (x, y), min_spacing):
-                        is_new_position_valid = False
-                        break
-                if is_new_position_valid:
-                    layout.append((x, y))
-                    break
-                attempts += 1
-            else:
-                raise RuntimeError("Maximum attempts reached without finding a valid position.")
-
+        layout = generate_random_incorrect_layout(n_turbines, area_size, min_spacing, max_attempts)
+        #layout = generate_random_correct_layout(n_turbines, area_size, min_spacing, max_attempts)
         encoded_layout = [encode_position_to_binary(pos[0], pos[1], num_bits) for pos in layout]
         initial_population.append(encoded_layout)
 
@@ -53,36 +29,42 @@ def genetic_algorithm(ga_config):
     n_turbines = ga_config['n_turbines']
     area_size = ga_config['area_size']
     min_spacing = ga_config['min_spacing']
-    max_attempts = ga_config['max_attempts']
     wind_speed = ga_config['wind_speed']
+    wind_direction = ga_config['wind_direction']
+    fitness_weights = ga_config['fitness_weights']
+
     population_size = ga_config['population_size']
     mutation_rate = ga_config['mutation_rate']
     max_generations = ga_config['max_generations']
     max_stagnation = ga_config['max_stagnation']
+    fitness_params = (fitness_weights, area_size, min_spacing, wind_speed, wind_direction)
 
-    solutions_fitness_values = []
+    solutions_max_fitness_values = []
+    solutions_avg_fitness_values = []
     solutions_layouts = []
     best_result = [float('-inf'), -1]
     best_result_generation = 0
     stagnation_counter = 0
 
-    num_bits = determine_num_bits(area_size)
-    population = initialize_population(population_size, n_turbines, area_size, min_spacing, max_attempts)
+    population = initialize_population(population_size, n_turbines, area_size, min_spacing)
+    global_best_solution = calculate_layout_energy_production(population[0], wind_speed)
 
     # main GA loop
     for generation in range(max_generations):
+        print(f'>>> Generation {generation}')
 
-        fitness_values = evaluate_fitness(population, area_size, min_spacing, wind_speed)
+        fitness_values = evaluate_fitness(population, *fitness_params)
 
         # Update best result
         current_best_result = max(zip(fitness_values, range(len(fitness_values))))
-        current_layout = [decode_binary_to_position(pos, num_bits) for pos in population[current_best_result[1]]]
-        solutions_fitness_values.append(current_best_result[0])
+        current_avg_result = sum(fitness_values) / len(fitness_values)
+        current_layout = decode_layout_to_position(population[current_best_result[1]])
+        solutions_max_fitness_values.append(current_best_result[0])
+        solutions_avg_fitness_values.append(current_avg_result)
         solutions_layouts.append(current_layout)
         if current_best_result[0] > best_result[0]:
-            best_result = current_best_result
-            best_result_generation = generation
-            print(f'>>> Generation {generation} best result {best_result[0]}')
+            best_result, best_result_generation = current_best_result, generation
+            print(f'>>>>>> new best result {best_result[0]}/{global_best_solution}')
             stagnation_counter = 0
         else:
             stagnation_counter += 1
@@ -99,24 +81,22 @@ def genetic_algorithm(ga_config):
         selected_parents = selection(population, fitness_values)
         offspring = crossover(selected_parents)
         mutated_offspring = mutation(offspring, mutation_rate, verbose=False)
-
-        # print("fitness_values", max(fitness_values), fitness_values.count(float('-inf')), end=" ")
-        # print_population(population, "population")
-        # print_population(selected_parents, "selected_parents:")
-        # print_population(offspring, "offspring")
-        # print_population(mutated_offspring, "mutated_offspring")
-
-        evaluate_fitness_params = (area_size, min_spacing, wind_speed)
-        population = replacement(population, mutated_offspring, fitness_values, evaluate_fitness_params)
-        # print_population(population, "replacement")
+        population = replacement(population, mutated_offspring, fitness_values, fitness_params)
+        # print_population(population)
 
     best_fitness = best_result[0]
-    best_layout = [decode_binary_to_position(pos, num_bits) for pos in population[best_result[1]]]
+    best_layout = decode_layout_to_position(population[best_result[1]])
     print(f"Best result found: P = {best_fitness} MW")
     print(f"Best turbines layout found: {best_layout}")
     print(f'Generation of best result {best_result_generation}')
     print('-' * 40)
-    # plot_turbine_layout(best_layout_pos)
+    # plot_turbine_layout(best_layout_pos, title="best_layout_pos")
+    # plot_turbine_layout(decode_layout_to_position(population[0]), title="Last generation 1st layout")
+    # plot_multiple_layouts([decode_layout_to_position(l) for l in population[::100]], title="Last generation layouts")
 
-    return (best_fitness, best_layout), (solutions_fitness_values, solutions_layouts)
+
+    return (global_best_solution,
+            (best_fitness, best_layout),
+            (solutions_max_fitness_values, solutions_layouts),
+            solutions_avg_fitness_values)
 
--- main.py
+++ main.py
@@ -7,54 +7,61 @@ from plotting import *
 # plt.rcParams["figure.dpi"] = 90
 
 
-ga_config = {
-    'n_turbines': 10,
-    'area_size': 1300,
-    'min_spacing': 100,
-    'max_attempts': 100,
-    'wind_speed': 9.8,
-    'population_size': 1000,
-    'mutation_rate': 0.1,
-    'max_generations': 100,
-    'max_stagnation': 50,
-}
+# PROBLEM DEFINITION
+n_turbines = 14
+area_size = 300
+min_spacing = 50
+wind_speed = 9.8
+wind_direction = 270.0
 
-ga_config_fast = {
-    'n_turbines': 5,
-    'area_size': 1000,
-    'min_spacing': 100,
-    'max_attempts': 100,
-    'wind_speed': 9.8,
+
+ga_config = {
+    'n_turbines': n_turbines,
+    'area_size': area_size,
+    'min_spacing': min_spacing,
+    'wind_speed': wind_speed,
+    'wind_direction': wind_direction,
     'population_size': 100,
-    'mutation_rate': 0.1,
-    'max_generations': 50,
-    'max_stagnation': 50,
+    'mutation_rate': 0.01,
+    'max_generations': 250,
+    'max_stagnation': 20,
+    'fitness_weights': {
+        'energy_production': 1,
+        'boundary_fitness': 1,
+        'spacing_fitness': 1,
+        'wake_fitness': 10,
+    }
 }
 
 
 if __name__ == '__main__':
     solutions_best_fitness_values = []
     solutions_best_layouts = []
-    solutions_fitness_values = []
+    solutions_max_fitness_values = []
+    solutions_avg_fitness_values = []
     solutions_layouts = []
-    for i in range(2):
+    global_best_solution = 0
+    for i in range(1):
         print(f"ITERATION {i+1}")
-        (best_fitness, best_layout), (fitness_values, layouts) = genetic_algorithm(ga_config)
-        # (best_fitness, best_layout), (fitness_values, layouts) = genetic_algorithm(ga_config_fast)
+        gbs, (best_fitness, best_layout), (fitness_max_values, layouts), fitness_avg_values = genetic_algorithm(ga_config)
         solutions_best_fitness_values.append([i+1, best_fitness])
         solutions_best_layouts.append(best_layout)
-        solutions_fitness_values.append(fitness_values)
+        solutions_max_fitness_values.append(fitness_max_values)
+        solutions_avg_fitness_values.append(fitness_avg_values)
         solutions_layouts.append(layouts)
-    print('\nsolutions_best_fitness_values: ', solutions_best_fitness_values)
+        global_best_solution = gbs
+    print('\nsolutions_best_fitness_values: ', solutions_best_fitness_values, global_best_solution)
 
-    plot_population_layouts(solutions_best_layouts, title=f'Best turbine layouts')
+    # plot_multiple_layouts(solutions_best_layouts, title=f'Best turbine layouts')
+    # for i, layouts in enumerate(solutions_layouts):
+    #     plot_multiple_layouts(layouts[::10], title=f'Turbine layouts {i + 1}', alpha_ascending=True)
 
-    for i, fitness_values in enumerate(solutions_fitness_values):
-        plt.plot(fitness_values, label=f'solution {i+1}')
-    plt.title(f'Fitness scores across generations {solutions_best_fitness_values}')
-    plt.legend(loc='lower right')
-    plt.show()
+    plot_solutions_data_stats(solutions_max_fitness_values,
+                              title='Fitness max scores across generations {solutions_best_fitness_values}',
+                              axhline=global_best_solution)
+    plot_solutions_data_stats(solutions_avg_fitness_values,
+                              title='Fitness average scores', axhline=global_best_solution)
 
-    for i, layouts in enumerate(solutions_layouts):
-        plot_population_layouts(layouts[::10], title=f'Turbine layouts {i+1}', alpha_ascending=True)
-
+    print('Are solutions_best_layouts valid?')
+    for i, layout in enumerate(solutions_best_layouts):
+        print(f"solution {i+1} valid:", is_layout_valid(layout, area_size, min_spacing))
